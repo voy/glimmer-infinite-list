4a78cd02ce761482b15cb58747a8c4d2df8d61bd
Vojtech Jasny <vojtech.jasny@gooddata.com>
2015-05-14 13:26:37 +0200

    Fixed typo

--- app/views/infinite-list-item.js
+++ app/views/infinite-list-item.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-    itemHeight: Em.computed.oneWay('parentView.itemHeight')
+    itemHeight: Ember.computed.oneWay('parentView.itemHeight')
 });
